c31b1a453f2d3f873fb92978ff6fc01bf3f5dd28
changping0823 <993195264@qq.com>
2020-02-16 21:58:37 +0800

    Create CartNum.dart

---
+++ lib/pages/Cart/CartNum.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import '../../services/ScreenAdapter.dart';
+
+class CartNum extends StatefulWidget {
+  CartNum({Key key}) : super(key: key);
+
+  @override
+  _CartNumState createState() => _CartNumState();
+}
+
+class _CartNumState extends State<CartNum> {
+  @override
+  Widget build(BuildContext context) {
+    ScreenAdapter.init(context);
+    return Container(
+      width: ScreenAdapter.width(160),
+      height: ScreenAdapter.height(40),
+      decoration:
+          BoxDecoration(border: Border.all(width: 1, color: Colors.black12)),
+      child: Row(
+        children: <Widget>[
+          _leftButton(),
+          _centerArea(),
+          _rightButton()
+        ],
+      ),
+    );
+  }
+
+  // 左边按钮
+  Widget _leftButton() {
+    return InkWell(
+      onTap: () {},
+      child: Container(
+        alignment: Alignment.center,
+        width: ScreenAdapter.width(45),
+        child: Text('-'),
+      ),
+    );
+  }
+
+  // 右边按钮
+  Widget _rightButton() {
+    return InkWell(
+      onTap: () {},
+      child: Container(
+        alignment: Alignment.center,
+        width: ScreenAdapter.width(45),
+        child: Text('+'),
+      ),
+    );
+  }
+
+  // 中间区域
+  Widget _centerArea() {
+    return Container(
+      alignment: Alignment.center,
+      width: ScreenAdapter.width(66),
+      decoration: BoxDecoration(
+          border: Border(
+        left: BorderSide(width: 1, color: Colors.black12),
+        right: BorderSide(width: 1, color: Colors.black12),
+      )),
+      child: Text('1'),
+    );
+  }
+}
