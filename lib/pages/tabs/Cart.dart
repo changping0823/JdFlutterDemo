9e1cbd87f141b9a9e1d57a9a7f5e5b8b2ff127a9
changping0823 <993195264@qq.com>
2020-02-16 20:46:44 +0800

    Update Cart.dart

--- lib/pages/tabs/Cart.dart
+++ lib/pages/tabs/Cart.dart
@@ -1,4 +1,8 @@
+
+
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../../Provider/Counter.dart';
 
 class CartPage extends StatefulWidget {
   CartPage({Key key}) : super(key: key);
@@ -8,8 +12,40 @@ class CartPage extends StatefulWidget {
 }
 
 class _CartPageState extends State<CartPage> {
+  
   @override
   Widget build(BuildContext context) {
-    return Text('我是购物车组件');
+    final counter = Provider.of<Counter>(context);
+    return Scaffold(
+      appBar: AppBar(
+        title:Text('购物车'),
+        actions: <Widget>[
+          IconButton(icon: Icon(Icons.edit), onPressed: null)
+        ],
+      ),
+      body: Center(
+        child: Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        const Text(
+          'You have pushed the button this many times:',
+        ),
+        Text(
+          '${counter.count}',
+          // ignore: deprecated_member_use
+          style: Theme.of(context).textTheme.display1,
+        ),
+      ],
+    )
+        ),
+      floatingActionButton:  FloatingActionButton(
+      onPressed: () {
+        Provider.of<Counter>(context, listen: false).increment();
+      },
+      tooltip: 'Increment',
+      child: const Icon(Icons.add),
+    ),
+    );
   }
 }
