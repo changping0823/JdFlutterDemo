d146e328baaac6d6984c1a4c9814ae1e261d0921
changping0823 <993195264@qq.com>
2020-02-16 20:46:58 +0800

    Update User.dart

--- lib/pages/tabs/User.dart
+++ lib/pages/tabs/User.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../../Provider/Counter.dart';
 
 class UserPage extends StatefulWidget {
   UserPage({Key key}) : super(key: key);
@@ -9,7 +11,32 @@ class UserPage extends StatefulWidget {
 
 class _UserPageState extends State<UserPage> {
   @override
-  Widget build(BuildContext context) {
-    return Text('我是我的组件');
+  Widget build(BuildContext context) {final counter = Provider.of<Counter>(context);
+    return Scaffold(
+      appBar: AppBar(title:Text('用户中心')),
+      body: Center(
+        child: Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        const Text(
+          'You have pushed the button this many times:',
+        ),
+        Text(
+          '${counter.count}',
+          // ignore: deprecated_member_use
+          style: Theme.of(context).textTheme.display1,
+        ),
+      ],
+    )
+        ),
+      floatingActionButton:  FloatingActionButton(
+      onPressed: () {
+        Provider.of<Counter>(context, listen: false).increment();
+      },
+      tooltip: 'Increment',
+      child: const Icon(Icons.add),
+    ),
+    );
   }
 }
