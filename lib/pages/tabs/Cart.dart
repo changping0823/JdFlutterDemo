4133a5afefef4e87d2d106ba392dc08568a1002b
changping0823 <993195264@qq.com>
2020-02-16 21:58:40 +0800

    Update Cart.dart

--- lib/pages/tabs/Cart.dart
+++ lib/pages/tabs/Cart.dart
@@ -1,8 +1,8 @@
-
-
 import 'package:flutter/material.dart';
+import '../../services/ScreenAdapter.dart';
 import 'package:provider/provider.dart';
 import '../../Provider/Counter.dart';
+import '../Cart/CartItem.dart';
 
 class CartPage extends StatefulWidget {
   CartPage({Key key}) : super(key: key);
@@ -12,40 +12,61 @@ class CartPage extends StatefulWidget {
 }
 
 class _CartPageState extends State<CartPage> {
-  
   @override
   Widget build(BuildContext context) {
+    ScreenAdapter.init(context);
     final counter = Provider.of<Counter>(context);
     return Scaffold(
       appBar: AppBar(
-        title:Text('购物车'),
-        actions: <Widget>[
-          IconButton(icon: Icon(Icons.edit), onPressed: null)
+        title: Text('购物车'),
+        actions: <Widget>[IconButton(icon: Icon(Icons.edit), onPressed: null)],
+      ),
+      body: Stack(
+        children: <Widget>[
+          ListView(
+            children: <Widget>[
+              CartItem(),
+              CartItem(),
+              CartItem(),
+              CartItem(),
+            ],
+          ),
+          Positioned(
+              bottom: ScreenAdapter.bottomSafeHeight(),
+              width: ScreenAdapter.width(750),
+              height: ScreenAdapter.height(88),
+              child: Stack(
+                children: <Widget>[
+                  Divider(),
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Row(
+                      children: <Widget>[
+                        Container(
+                          width: ScreenAdapter.width(60),
+                          child: Checkbox(
+                              value: true,
+                              activeColor: Colors.pink,
+                              onChanged: (check) {}),
+                        ),
+                        Text('全选')
+                      ],
+                    ),
+                  ),
+                  Align(
+                    alignment: Alignment.centerRight,
+                    child: RaisedButton(
+                      color: Colors.red,
+                      child: Text('结算',style: TextStyle(color: Colors.white)),
+                      onPressed:(){
+
+                      }
+                    ),
+                  )
+                ],
+              ))
         ],
       ),
-      body: Center(
-        child: Column(
-      mainAxisSize: MainAxisSize.min,
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        const Text(
-          'You have pushed the button this many times:',
-        ),
-        Text(
-          '${counter.count}',
-          // ignore: deprecated_member_use
-          style: Theme.of(context).textTheme.display1,
-        ),
-      ],
-    )
-        ),
-      floatingActionButton:  FloatingActionButton(
-      onPressed: () {
-        Provider.of<Counter>(context, listen: false).increment();
-      },
-      tooltip: 'Increment',
-      child: const Icon(Icons.add),
-    ),
     );
   }
 }
