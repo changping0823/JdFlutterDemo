b4c99cea9a6c50867fceff091ef7658d9b5c3fc4
changping0823 <993195264@qq.com>
2020-02-17 16:01:02 +0800

    Update CartCounter.dart

--- lib/Provider/CartCounter.dart
+++ lib/Provider/CartCounter.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'dart:convert';
+
+import '../services/Storage.dart';
 
 class CartCounter with ChangeNotifier {
   List _cartList = [];
@@ -6,8 +9,26 @@ class CartCounter with ChangeNotifier {
   List get cartList=>this._cartList;
   int get cartNum=>this._cartList.length;
 
+  CartCounter(){
+    this.init();
+  }
+  
+  init() async{
+    try {
+      List cartListData = json.decode(await Storage.getString('cartList'));
+      this._cartList = cartListData;
+    } catch (e) {
+      this._cartList = [];
+    }
+    notifyListeners();
+  }
+
   addList(value) {
     this._cartList.add(value);
     notifyListeners();
+  }
+
+  updateCartList(){
+    this.init();
   }
 }
