c018f0e047c4034e66e009764b3307031aa6f2e3
changping0823 <993195264@qq.com>
2020-02-17 16:00:57 +0800

    Update Cart.dart

--- lib/pages/tabs/Cart.dart
+++ lib/pages/tabs/Cart.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import '../../services/ScreenAdapter.dart';
 import 'package:provider/provider.dart';
-import '../../Provider/Counter.dart';
+import '../../Provider/CartCounter.dart';
 import '../Cart/CartItem.dart';
 
 class CartPage extends StatefulWidget {
@@ -15,56 +15,60 @@ class _CartPageState extends State<CartPage> {
   @override
   Widget build(BuildContext context) {
     ScreenAdapter.init(context);
-    final counter = Provider.of<Counter>(context);
+    var cartCounter = Provider.of<CartCounter>(context);
+
     return Scaffold(
       appBar: AppBar(
         title: Text('购物车'),
         actions: <Widget>[IconButton(icon: Icon(Icons.edit), onPressed: null)],
       ),
       body: Stack(
+        
         children: <Widget>[
-          ListView(
+          cartCounter.cartList.length > 0?ListView(
             children: <Widget>[
-              CartItem(),
-              CartItem(),
-              CartItem(),
-              CartItem(),
+              Column(
+                children: cartCounter.cartList.map((value){
+                  return CartItem(value);
+                }).toList(),
+              ),
+              SizedBox(height: ScreenAdapter.height(88))
             ],
-          ),
+          ):Center(child: Text('暂无数据')),
           Positioned(
-              bottom: ScreenAdapter.bottomSafeHeight(),
-              width: ScreenAdapter.width(750),
-              height: ScreenAdapter.height(88),
-              child: Stack(
-                children: <Widget>[
-                  Divider(),
-                  Align(
-                    alignment: Alignment.centerLeft,
-                    child: Row(
-                      children: <Widget>[
-                        Container(
-                          width: ScreenAdapter.width(60),
-                          child: Checkbox(
-                              value: true,
-                              activeColor: Colors.pink,
-                              onChanged: (check) {}),
-                        ),
-                        Text('全选')
-                      ],
+            bottom: ScreenAdapter.bottomSafeHeight(),
+            width: ScreenAdapter.width(750),
+            height: ScreenAdapter.height(88),
+            child: Container(
+                color: Colors.white,
+                child: Stack(
+                  children: <Widget>[
+                    Align(
+                      alignment: Alignment.centerLeft,
+                      child: Row(
+                        children: <Widget>[
+                          Container(
+                            width: ScreenAdapter.width(60),
+                            child: Checkbox(
+                                value: true,
+                                activeColor: Colors.pink,
+                                onChanged: (check) {}),
+                          ),
+                          Text('全选')
+                        ],
+                      ),
                     ),
-                  ),
-                  Align(
-                    alignment: Alignment.centerRight,
-                    child: RaisedButton(
-                      color: Colors.red,
-                      child: Text('结算',style: TextStyle(color: Colors.white)),
-                      onPressed:(){
-
-                      }
-                    ),
-                  )
-                ],
-              ))
+                    Align(
+                      alignment: Alignment.centerRight,
+                      child: RaisedButton(
+                          color: Colors.red,
+                          child:
+                              Text('结算', style: TextStyle(color: Colors.white)),
+                          onPressed: () {}),
+                    )
+                  ],
+                )),
+          )
         ],
       ),
     );
