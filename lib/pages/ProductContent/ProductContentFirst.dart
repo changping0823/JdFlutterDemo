b790fa3f40732397e2d1cdcbd4e76b848b5596eb
changping0823 <993195264@qq.com>
2020-02-17 16:00:47 +0800

    Update ProductContentFirst.dart

--- lib/pages/ProductContent/ProductContentFirst.dart
+++ lib/pages/ProductContent/ProductContentFirst.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../../Provider/CartCounter.dart';
+import '../../services/CartService.dart';
 import '../../Config/Config.dart';
 import '../../Models/ProductContentModel.dart';
 import '../../widget/JdButton.dart';
+import 'ProductNum.dart';
 import '../../services/ScreenAdapter.dart';
+import '../../services/EventBus.dart';
 
 class ProductContentFirst extends StatefulWidget {
   final ProductContentItem _productContent;
@@ -15,15 +20,32 @@ class ProductContentFirst extends StatefulWidget {
 class _ProductContentFirstState extends State<ProductContentFirst>
     with AutomaticKeepAliveClientMixin {
   List<Attr> _attr = [];
+  ProductContentItem _productContent;
   String _selectedAttr;
+  var actionEventBus;
+  CartCounter _cartCounter;
 
   bool get wantKeepAlive => true;
 
   @override
   void initState() {
     super.initState();
-    this._attr = widget._productContent.attr;
+    this._productContent = widget._productContent;
+    this._attr = this._productContent.attr;
     _initAttr();
+    
+    /// 监听广播
+    this.actionEventBus = eventBus.on<ProductContentEvent>().listen((event) {
+      this._attrBottomSheet();
+    });
+  }
+
+  void dispose() {
+    super.dispose();
+    print('dispose');
+
+    /// 取消监听事件
+    this.actionEventBus.cancel();
   }
 
   _initAttr() {
@@ -35,11 +57,10 @@ class _ProductContentFirstState extends State<ProductContentFirst>
       }
     }
 
-     _getSelectedAttrValue();
-
+    _getSelectedAttrValue();
   }
 
-   _changeAttr(cate, title,setBottomState) {
+  _changeAttr(cate, title, setBottomState) {
     List<Attr> attr = this._attr;
     for (Attr item in attr) {
       if (item.cate == cate) {
@@ -54,13 +75,13 @@ class _ProductContentFirstState extends State<ProductContentFirst>
 
     _getSelectedAttrValue();
   }
-  
-  _getSelectedAttrValue(){
+
+  _getSelectedAttrValue() {
     var attr = this._attr;
     List tempArr = [];
     for (var item in attr) {
       for (var a in item.attrList) {
-        if(a['checked'] == true){
+        if (a['checked'] == true) {
           tempArr.add(a['title']);
         }
       }
@@ -68,6 +89,7 @@ class _ProductContentFirstState extends State<ProductContentFirst>
     // print(tempArr);
     setState(() {
       this._selectedAttr = tempArr.join(',');
+      this._productContent.selectedAttr = this._selectedAttr;
     });
   }
 
@@ -103,10 +125,20 @@ class _ProductContentFirstState extends State<ProductContentFirst>
                                                 fontWeight: FontWeight.bold)),
                                       ),
                                     ),
-                                    _attrWidget(value,setBottomState)
+                                    _attrWidget(value, setBottomState)
                                   ],
                                 );
-                              }).toList())
+                              }).toList()),
+                          Divider(),
+                          Row(
+                            children: <Widget>[
+                              Text("数量: ",
+                                  style:
+                                      TextStyle(fontWeight: FontWeight.bold)),
+                              SizedBox(width: 10),
+                              ProductNum(this._productContent)
+                            ],
+                          )
                         ],
                       ),
                     ),
@@ -115,7 +147,6 @@ class _ProductContentFirstState extends State<ProductContentFirst>
                       width: ScreenAdapter.width(750),
                       height: ScreenAdapter.height(100),
                       child: Row(
-                        
                         children: <Widget>[
                           Expanded(
                             flex: 1,
@@ -124,8 +155,15 @@ class _ProductContentFirstState extends State<ProductContentFirst>
                               child: JdButton(
                                 color: Color.fromRGBO(253, 1, 0, 0.9),
                                 text: "加入购物车",
-                                action: () {
-                                  print('加入购物车');
+                                action: () async{
+                                  print('22222加入购物车');
+
+                                  await CartService.addProduct(this._productContent);
+                                  /// 关闭筛选弹出框
+                                  Navigator.pop(context);
+                                  ///调用provider更新数据
+                                  this._cartCounter.updateCartList();
+
                                 },
                               ),
                             ),
@@ -153,19 +191,20 @@ class _ProductContentFirstState extends State<ProductContentFirst>
         });
   }
 
-  _attrWidget(Attr value,setBottomState) {
+  _attrWidget(Attr value, setBottomState) {
     return Container(
       width: ScreenAdapter.width(610),
       child: Wrap(
         children: value.attrList.map((item) {
           return InkWell(
             onTap: () {
-              _changeAttr(value.cate, item['title'],setBottomState);
+              _changeAttr(value.cate, item['title'], setBottomState);
             },
             child: Container(
               margin: EdgeInsets.all(10),
               child: Chip(
-                label: Text("${item['title']}",style: TextStyle(color: Colors.white)),
+                label: Text("${item['title']}",
+                    style: TextStyle(color: Colors.white)),
                 padding: EdgeInsets.all(10),
                 backgroundColor: item['checked'] ? Colors.red : Colors.black26,
               ),
@@ -176,13 +215,13 @@ class _ProductContentFirstState extends State<ProductContentFirst>
     );
   }
 
- 
-
   @override
   Widget build(BuildContext context) {
 
+    this._cartCounter = Provider.of<CartCounter>(context);
+
     ScreenAdapter.init(context);
-    String pic = Config.domain + widget._productContent.pic;
+    String pic = Config.domain + this._productContent.pic;
     pic = pic.replaceAll('\\', '/');
     return Container(
       padding: EdgeInsets.all(10),
@@ -195,13 +234,13 @@ class _ProductContentFirstState extends State<ProductContentFirst>
           //标题
           Container(
             padding: EdgeInsets.only(top: 10),
-            child: Text("${widget._productContent.title}",
+            child: Text("${this._productContent.title}",
                 style: TextStyle(
                     color: Colors.black87, fontSize: ScreenAdapter.size(36))),
           ),
           Container(
               padding: EdgeInsets.only(top: 10),
-              child: Text("${widget._productContent.subTitle}",
+              child: Text("${this._productContent.subTitle}",
                   style: TextStyle(
                       color: Colors.black54,
                       fontSize: ScreenAdapter.size(28)))),
@@ -215,7 +254,7 @@ class _ProductContentFirstState extends State<ProductContentFirst>
                   child: Row(
                     children: <Widget>[
                       Text("特价: "),
-                      Text("¥${widget._productContent.price}",
+                      Text("¥${this._productContent.price}",
                           style: TextStyle(
                               color: Colors.red,
                               fontSize: ScreenAdapter.size(46))),
@@ -228,7 +267,7 @@ class _ProductContentFirstState extends State<ProductContentFirst>
                     mainAxisAlignment: MainAxisAlignment.end,
                     children: <Widget>[
                       Text("原价: "),
-                      Text("¥${widget._productContent.oldPrice}",
+                      Text("¥${this._productContent.oldPrice}",
                           style: TextStyle(
                               color: Colors.black38,
                               fontSize: ScreenAdapter.size(28),
@@ -240,21 +279,24 @@ class _ProductContentFirstState extends State<ProductContentFirst>
             ),
           ),
           //筛选
-          this._attr.length > 0?Container(
-            margin: EdgeInsets.only(top: 10),
-            height: ScreenAdapter.height(80),
-            child: InkWell(
-              onTap: () {
-                _attrBottomSheet();
-              },
-              child: Row(
-                children: <Widget>[
-                  Text("已选: ", style: TextStyle(fontWeight: FontWeight.bold)),
-                  Text("${this._selectedAttr}")
-                ],
-              ),
-            ),
-          ):Text(''),
+          this._attr.length > 0
+              ? Container(
+                  margin: EdgeInsets.only(top: 10),
+                  height: ScreenAdapter.height(80),
+                  child: InkWell(
+                    onTap: () {
+                      _attrBottomSheet();
+                    },
+                    child: Row(
+                      children: <Widget>[
+                        Text("已选: ",
+                            style: TextStyle(fontWeight: FontWeight.bold)),
+                        Text("${this._selectedAttr}")
+                      ],
+                    ),
+                  ),
+                )
+              : Text(''),
           Divider(),
           Container(
             height: ScreenAdapter.height(80),
