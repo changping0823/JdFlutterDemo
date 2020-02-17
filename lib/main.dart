7616fa4660883ce9336782d31d74f8c79bac6097
changping0823 <993195264@qq.com>
2020-02-17 16:00:31 +0800

    Update main.dart
    加入购物车功能

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'routers/router.dart';
-import 'Provider/Counter.dart';
 import 'Provider/CartCounter.dart';
 
 void main() => runApp(MyApp());
@@ -18,7 +17,6 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MultiProvider(
         providers: [
-          ChangeNotifierProvider(create: (_) => Counter()),
           ChangeNotifierProvider(create: (_) => CartCounter()),
         ],
         child: MaterialApp(
